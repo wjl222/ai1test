e77e95c6f30bc3907c3a0c4c53efc9ee83f143e8
wjl111 <5393782+wjl111@user.noreply.gitee.com>
2026-03-09 21:51:39 +0800

    next入门第九章节

--- day4-prepare/nextjs-dashboard/next.config.ts
+++ day4-prepare/nextjs-dashboard/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.devtool = 'source-map'; // 开启source map
+    return config;
+  },
 };
 
 export default nextConfig;
