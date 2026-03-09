ba85ad3ecc326f60ea32a588e98f8d9e5d7c4cae
wujialin <wujialin@broadtext.com>
2026-03-09 17:23:01 +0800

    next入门5-8章

--- day4-prepare/nextjs-dashboard/app/lib/data.ts
+++ day4-prepare/nextjs-dashboard/app/lib/data.ts
@@ -9,10 +9,15 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+// import { unstable_noStore as noStore } from 'next/cache';
+
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
+      // noStore();
+
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
