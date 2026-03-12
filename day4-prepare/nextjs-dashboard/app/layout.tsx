f5afad3b00d46e410c8dbf80b5f43f08aafffa23
wujialin <wujialin@broadtext.com>
2026-03-12 15:26:55 +0800

    tailwindcss练习

--- day4-prepare/nextjs-dashboard/app/layout.tsx
+++ day4-prepare/nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,10 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -7,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
